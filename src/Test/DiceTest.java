ce49800cea602a12969d1e37d7502fb7cf490f81
Michael Westergaard <michaelwestergaard@hotmail.dk>
2017-10-10 15:29:09 +0200

    Added JUnit test

---
+++ src/Test/DiceTest.java
@@ -0,0 +1,29 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiceTest {
+
+	@Test
+	public void testRollDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiceSum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSum() {
+		fail("Not yet implemented");
+	}
+
+}
