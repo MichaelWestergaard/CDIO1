b553fff96814fb7a54e5c6824c479093263bcbac
Alparslan B. Ari <burhanari@192.168.0.11>
2017-10-11 13:54:57 +0200

    Added player JUnit

---
+++ src/Test/PlayerTest.java
@@ -0,0 +1,39 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPoints() {
+		fail("Not yet implemented");
+	}
+
+}
