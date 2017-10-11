0033519989b397d6d3c0eab8c156f97dbbdcb212
Mr. Timn <Mr. Timn@192.168.91.1>
2017-10-11 21:39:46 +0200

    JUnit - testGetEns - Hvor ofte er de ens og hvad er sandsynligheden.

--- src/Test/DiceTest.java
+++ src/Test/DiceTest.java
@@ -10,7 +10,8 @@ public class DiceTest {
 
 Dice testDice = new Dice();
 	
-	@Test
+	@Test	//Test af 'raflebægeret' (diceRoll funktionen) hen over 1000 kast,
+			//samt hvorvidt de stemmer overens med den teoretiske sandsynlighed.
 	public void diceRollTest() {
 		int[] rolls = new int[1000];
 		int twoCount = 0, threeCount = 0, fourCount = 0, fiveCount = 0, sixCount = 0, sevenCount = 0, eightCount = 0, nineCount = 0,
@@ -57,21 +58,23 @@ Dice testDice = new Dice();
 			}
 		}
 		
-		System.out.println("2: " + twoCount + " ~ " + (double)twoCount/1000*100);  
-		System.out.println("3: " + threeCount + " ~ " + (double)threeCount/1000*100); 
-		System.out.println("4: " + fourCount + " ~ " + (double)fourCount/1000*100);  
-		System.out.println("5: " + fiveCount + " ~ " + (double)fiveCount/1000*100);  
-		System.out.println("6: " + sixCount + " ~ " + (double)sixCount/1000*100);   
-		System.out.println("7: " + sevenCount + " ~ " + (double)sevenCount/1000*100);
-		System.out.println("8: " + eightCount + " ~ " + (double)eightCount/1000*100);
-		System.out.println("9: " + nineCount + " ~ " + (double)nineCount/1000*100);
-		System.out.println("10: " + tenCount + " ~ " + (double)tenCount/1000*100);
-		System.out.println("11: " + elevenCount + " ~ " + (double)elevenCount/1000*100);
-		System.out.println("12: " + twelveCount + " ~ " + (double)twelveCount/1000*100);
+		System.out.println("2: " + twoCount + " ~ " + (double)twoCount/1000*100);			//Sandsynligheden bør være 2,78%
+		System.out.println("3: " + threeCount + " ~ " + (double)threeCount/1000*100);		//Sandsynligheden bør være 5,56% 
+		System.out.println("4: " + fourCount + " ~ " + (double)fourCount/1000*100);  		//Sandsynligheden bør være 8,33%
+		System.out.println("5: " + fiveCount + " ~ " + (double)fiveCount/1000*100);  		//Sandsynligheden bør være 11,11%
+		System.out.println("6: " + sixCount + " ~ " + (double)sixCount/1000*100);   		//Sandsynligheden bør være 13,89%
+		System.out.println("7: " + sevenCount + " ~ " + (double)sevenCount/1000*100);		//Sandsynligheden bør være 16,67%
+		System.out.println("8: " + eightCount + " ~ " + (double)eightCount/1000*100);		//Sandsynligheden bør være 13,89%
+		System.out.println("9: " + nineCount + " ~ " + (double)nineCount/1000*100);			//Sandsynligheden bør være 11,11%
+		System.out.println("10: " + tenCount + " ~ " + (double)tenCount/1000*100);			//Sandsynligheden bør være 8,33%
+		System.out.println("11: " + elevenCount + " ~ " + (double)elevenCount/1000*100);	//Sandsynligheden bør være 5,56%
+		System.out.println("12: " + twelveCount + " ~ " + (double)twelveCount/1000*100);	//Sandsynligheden bør være 2,78%
 		
 		
 		boolean twoTrue = false, threeTrue = false, fourTrue = false, fiveTrue = false, sixTrue = false, sevenTrue = false, eightTrue = false,
 				nineTrue = false, tenTrue = false, elevenTrue = false, twelveTrue = false;
+		
+		//Der er en tilladt afvigelse på 3% for sandsynligheden eftersom der altid vil være afvigelser, især med mindre værdier såsom 1000.
 		
 		if(twoCount >= 0 && twoCount <= 58) {
 			twoTrue = true;
@@ -112,10 +115,6 @@ Dice testDice = new Dice();
 			fail("Dice roll does not meet the requirements.");
 		}
 		
-			
-		
-		
-		
 	}
 
 	@Test
@@ -123,9 +122,48 @@ Dice testDice = new Dice();
 		fail("Not yet implemented");
 	}
 
-	@Test
+	
+	
+	
+	@Test	//Test af hvor ofte terningerne har samme værdi, samt hvorvidt antallet stemmer overens med den teoretiske sandsynlighed.
 	public void testGetEns() {
-		fail("Not yet implemented");
+		int ensCount = 0, forskelligCount = 0;
+		
+		for(int i=0; i < 1000; i++) {
+			
+			testDice.rollDice();
+			boolean ens = testDice.getEns();
+			
+			if(ens == true) {
+				ensCount++;
+			}
+			else{
+				forskelligCount++;
+			}
+			
+		}
+		
+		System.out.println("Ens: " + ensCount + " ~ " + (double)ensCount/1000*100); 						//Sandsynligheden bør være 16,67%
+		System.out.println("Forskellig: " + forskelligCount + " ~ " + (double)forskelligCount/1000*100); 	//Sandsynligheden bør være 83,33%
+		
+		boolean ensTrue = false, forskelligTrue = false;
+		
+		//Tilladt afvigelse 3%.
+		
+		if(ensCount >= 137 && ensCount <= 197) {
+			ensTrue = true;
+		}
+		if(forskelligCount >= 803 && forskelligCount <= 863) {
+			forskelligTrue = true;
+		}
+		
+		if(ensTrue == false || forskelligTrue == false) {
+			fail("The probability doesn't match the expected value");
+		}
+		
+	
+		
+		
 	}
 
 	@Test
